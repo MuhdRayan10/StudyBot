39e89df48a045cd6587415dd30d743c9c1d8414a
MuhdRayan10 <muhammedrayansavad1@gmail.com>
2023-05-27 12:33:58 +0300

    fixed subject class

--- subject.py
+++ subject.py
@@ -1,21 +1,18 @@
-import Chapter
+from chapter import Chapter
 
 class Subject:
-    def __init__(self, subject_name, subject_code):
-        self.subject_name = subject_name
-        self.subject_code = subject_code
+    def __init__(self, name:str, code:str):
+        self.name = name
+        self.subject_code = code
 
         # chapters
         self.chapters = []
 
-    def add_chapter(self, chapter: Chapter) -> None:
+    def add_chapter(self, chapter:Chapter) -> None:
         self.chapters.append(chapter)
     
-    def get_chapter(self, chapter_name:str) -> Chapter:
-        return [chapter for chapter in self.chapters if chapter.chapter_name == chapter_name][0]
-
-    def __str__(self):
-        return f"{self.subject_name}({self.subject_code})"
+    def get_chapter(self, name:str) -> Chapter:
+        return [chapter for chapter in self.chapters if Chapter.name == name][0]
 
     def __repr__(self):
         return f"{self.subject_name}({self.subject_code})"
