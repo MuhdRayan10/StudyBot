db0a7b0b6306df3fdfe852864be9a74bc2b8bd1d
MuhdRayan10 <muhammedrayansavad1@gmail.com>
2023-05-28 13:54:15 +0300

    get_time_left func
    added new function that calculates time till exams commence

--- exam.py
+++ exam.py
@@ -1,13 +1,25 @@
+from datetime import date
+
+
 class Exam:
-    def __init__(self, name:str, marks:int, chapters:list, grade:int, date:tuple):
+    def __init__(self, name:str, marks:int, chapters:list, grade:int, d:tuple):
         self.exam = name
         self.marks = marks
         self.grade = grade
 
         self.chapters = chapters
-        self.date = date
+        self.date = d
 
     def get_time_left(self):
-        pass
+        exam = date(*self.date)
+        today = date.today()
 
+        time_left = exam - today
+        print(time_left)
+
+        return time_left.days
     
+
+half_yearly = Exam("Half Yearly", 80, ['Light', 'Chemical Reactions'], 10, (2023, 5, 30))
+print(half_yearly.get_time_left())
+
