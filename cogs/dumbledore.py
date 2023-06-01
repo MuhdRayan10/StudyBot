76517d2179ac0034d50f4367788066a992f57e97
MuhdRayan10 <muhammedrayansavad1@gmail.com>
2023-06-01 23:47:11 +0300

    drop down + add_chapter() command

--- cogs/dumbledore.py
+++ cogs/dumbledore.py
@@ -1,21 +1,36 @@
-from discord import app_commands
+from discord import app_commands, SelectOption as opt
 from discord.ext import commands
+from discord.ui import Select, View
 import discord
 import sqlite3
 
-class DumbleDore:
+class DumbleDore(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name='register', description="Start managing your studies!")
     async def register(self, interaction:discord.Interaction, grade:int):
+        
+        await interaction.response.defer()
+        
         db = sqlite3.connect("./data/users.db")
         c = db.cursor()
 
         c.execute("CREATE TABLE IF NOT EXISTS users (name TEXT, grade INTEGER, subjects TEXT, strengths TEXT, weaknesses TEXT, breaks TEXT, holidays TEXT)")
         db.commit()
 
-        await interaction.response.send_message("Great!")
+        grades = Select(min_values=1, options=[opt(label="IX", description="Grade 9"), opt(label="X", description="Grade 10")])
+        grades.callback = self.grade_clicked
+
+        view = View()
+        view.add_item(grades)
+        
+        await interaction.followup.send("Choose your grade!", view=view)
+
+    async def grade_clicked(self, interaction):
+        await interaction.response.send_message("grade")
+
+
 
 async def setup(bot):
     await bot.add_cog(DumbleDore(bot))
--- database.py
+++ database.py
@@ -1,33 +1,42 @@
 import sqlite3
 
-def convert_list(chapters:list):
-    li = [chapter.data_list() for chapter in chapters]
-
+def convert_chapters(chapters:list):
+    return [chapter.data_list() for chapter in chapters]
 
 def subj_setup(grade:int):
     db = sqlite3.connect("./data/subjects")
     c = db.cursor()
 
     c.execute(f"CREATE TABLE IF NOT EXISTS subj_{grade} (name TEXT, chapters TEXT)")
+    db.commit()
+
+    return db, c
 
 
 def add_subect(grade:int, subjects:list):
-    db = sqlite3.connect("./data/subjects")
-    c = db.cursor()
-
-    c.execute(f"CREATE TABLE IF NOT EXISTS subj_{grade} (name TEXT, chapters TEXT)")
-    db.commit()
+    db, c = subj_setup(grade)
 
     for subject in subjects:
-        c.execute(f"INSERT INTO subj_{grade} VALUES (?, ?)", (subject.name, convert_list(subject.chapters)))
+        c.execute(f"INSERT INTO subj_{grade} VALUES (?, ?)", (subject.name, str(convert_chapters(subject.chapters))))
 
     db.commit()
     db.close()
 
 def add_chapter(grade:int, subject:str, chapters:list):
-    db = sqlite3.connect("./data/subjects")
-    c = db.cursor()
+    db, c = subj_setup(grade)
 
+    c.execute(f"SELECT chapter FROM subj_{grade} WHERE name='{subject}'")
+    data = c.fetchall()
+
+    if not data: return
+
+    ch = eval(data[0][1])
+    ch.extend(convert_chapters(chapters))
+
+    c.execute(f"UPDATE subj_{grade} SET chapters=:c WHERE name=:n", {"c":str(ch), "n":subject})
+    db.commit()
+
+    db.close()
 
 
 
